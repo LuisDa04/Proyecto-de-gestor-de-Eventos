b76aff52b211447826d7491b80b50cab4ae0fc04
LuisDa <luisdavidgilquintana.com>
2025-10-10 11:06:17 -0400

    volvemos a lo generico

--- Enums.py
+++ Enums.py
@@ -1,15 +1,22 @@
 from enum import Enum
 
 class TipoRecurso(Enum):
-    QUIROFANO = "Quirófano"
-    EQUIPO_MEDICO = "Equipo Médico"
-    PERSONAL_MEDICO = "Personal Médico"
-    SALA = "Sala"
-    INSTRUMENTAL = "Instrumental"
+    QUIROFANO = "Quirófano" #para cirugia
+    CIRUJANO = "Cirujano" #para quirofano
+    FISIOTERAPEUTA = "Fisioterapeuta" #para fisioterapia
+    DOCTOR = "Doctor" #todos los eventos necesitan un doctor, excepto las consultas y fisioterapia
+    ENFERMEROS = "Enfermeros" #todos los eventos necesitan enfermeros
+    CONSULTA = "Consulta" #sala de consultas (inculye urgencias)
+    UCI = "UCI" #para terapia
+    REHABILITACION_FISICA = "Sala de rehabilitacion fisica" #para fisioterapia
+    INSTRUMENTAL = "Instrumental" #equipamiento basico necesario para tratar con cada paciente, infinito
+    EXAMENES = "Sala de examenes" #necesaria para el examen medico
+    CAMILLA = "Camilla" #cada sala tiene una cantidad limitada de camillas (Quirofano, Consulta, UCI, Rehabilitacion fisica)
 
 class TipoEvento(Enum):
     CIRUGIA = "Cirugía"
-    CONSULTA = "Consulta"
-    EXAMEN = "Examen Médico"
+    CONSULTA = "Consulta" 
+    EXAMEN = "Examen Médico" #evento necesario antes de cirugia (en este caso toma prioridad)
     TERAPIA = "Terapia"
-    URGENCIA = "Urgencia"
+    FISIOTERAPIA = "Fisioterapia"
+    URGENCIA = "Urgencia" #prioridad sobre los demas eventos en caso de falta de recursos (cuenta como consulta)
